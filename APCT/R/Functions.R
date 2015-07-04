6432240b86570b139b29cd54d3335ed33c13ef1e
Tim Riffe <tim.riffe@gmail.com>
2015-07-05 00:07:06 +0200

    move modified function to functions script. Consider package if functions accumulate.

--- APCT/R/Functions.R
+++ APCT/R/Functions.R
@@ -6,51 +6,51 @@
 
 
 #'
-#' @title LexRef5 Overlays semi-transparent 5-year lexis triangle outlines onto a Lexis plot
+#' @title LexRefN Overlays semi-transparent N-year lexis triangle outlines onto a Lexis plot
 #' 
 #' @description This is an optional overlay function for Lexis map plots.
 #' 
 #' @param ages vector of ages, e.g. \code{0:110}. These are y values.
 #' @param years vector of years, e.g. \code{1900:2010}. These are x values.
-#' @param col line color, default \code{"#DDDDDD50"} (transparent gray).
+#' @param N. age interval. Default of 5.
+#' @param Chrono logical. Default \code{TRUE}. Birth cohorts? If \code{FALSE} it assumes we have a TPD diagram and gives death cohorts.
 #' @param ... optional arguments passed to \code{segments()}
 #' 
 #' @export
 #' 
 
-LexRefN <- function(ages, years, N = 5, col = "#DDDDDD50", ...){
+LexRefN <- function(ages, years, N = 5, Chrono = TRUE, ...){
 	# vertical
-	segments(years[years %% 5 == 0],
+	segments(years[years %% N == 0],
 			min(ages),
-			years[years %% 5 == 0],
-			max(ages) + 1,
-			col = col,
+			years[years %% N == 0],
+			max(ages),
 			...
+	
 	)
-	# horiz lexis lines
+	# horizontal lines
 	segments(min(years),
-			ages[ages %% 5 == 0],
-			max(years) + 1,
-			ages[ages %% 5 == 0],
-			col = col,
+			ages[ages %% N == 0],
+			max(years),
+			ages[ages %% N == 0],
 			...
 	)
 	# diag cohort references, bit more tricky:
 	l.years <- (min(years) - ages) 
-	coh.ext <- range(c(l.years[l.years %% 5 == 0], years[years %% 5 == 0]))
-	cohs  <- seq(coh.ext[1], coh.ext[2], by = 5)
+	coh.ext <- range(c(l.years[l.years %% N == 0], years[years %% N == 0]))
+	cohs    <- seq(coh.ext[1], coh.ext[2], by = N)
 	
 	# bottom, left:
 	xl  <- cohs + min(ages)
-	yb  <- rep(min(ages), length(cohs))
+	yb <- rep(min(ages), length(cohs))
 	yb[xl < min(years)] <- yb[xl < min(years)] + min(years) - xl[xl < min(years)]
 	xl[xl < min(years)] <- min(years)
 	
 	# top, right:
-	xr  <- cohs + max(ages) + 1
-	yt  <- rep(max(ages) + 1, length(cohs))
-	yt[xr > (max(years) + 1)] <- yt[xr > (max(years) + 1)] - xr[xr > (max(years) + 1)] + (max(years) + 1)
-	xr[xr > (max(years) + 1)] <- max(years) + 1
+	xr  <- cohs + max(ages)
+	yt  <- rep(max(ages), length(cohs))
+	yt[xr > max(years)] <- yt[xr > max(years)] - xr[xr > max(years)] + max(years)
+	xr[xr > max(years)] <- max(years)
 	
 	# cut down one last time:
 	xr <- xr[yt >= min(ages)]
@@ -59,7 +59,12 @@ LexRefN <- function(ages, years, N = 5, col = "#DDDDDD50", ...){
 	yt <- yt[yt >= min(ages)]
 	
 	# draw cohort refs:
-	segments(xl, yb, xr, yt, col = col, ...)
+	if (Chrono){
+		segments(xl, yb, xr, yt, ...)
+	} else {
+		segments(rev(xr), yb, rev(xl), yt, ...)
+	}
+	
 }
 
 
--- APCT/R/LexisStandard.R
+++ APCT/R/LexisStandard.R
@@ -16,54 +16,7 @@ getwd()
 
 
 #library(LexisUtils)
-
-LexRefN <- function(ages, years, N = 5, Chrono = TRUE, ...){
-	# vertical
-	segments(years[years %% N == 0],
-			min(ages),
-			years[years %% N == 0],
-			max(ages),
-			...
-	
-	)
-	# horizontal lines
-	segments(min(years),
-			ages[ages %% N == 0],
-			max(years),
-			ages[ages %% N == 0],
-			...
-	)
-	# diag cohort references, bit more tricky:
-	l.years <- (min(years) - ages) 
-	coh.ext <- range(c(l.years[l.years %% N == 0], years[years %% N == 0]))
-	cohs    <- seq(coh.ext[1], coh.ext[2], by = N)
-	
-	# bottom, left:
-	xl  <- cohs + min(ages)
-	yb <- rep(min(ages), length(cohs))
-	yb[xl < min(years)] <- yb[xl < min(years)] + min(years) - xl[xl < min(years)]
-	xl[xl < min(years)] <- min(years)
-	
-	# top, right:
-	xr  <- cohs + max(ages)
-	yt  <- rep(max(ages), length(cohs))
-	yt[xr > max(years)] <- yt[xr > max(years)] - xr[xr > max(years)] + max(years)
-	xr[xr > max(years)] <- max(years)
-	
-	# cut down one last time:
-	xr <- xr[yt >= min(ages)]
-	xl <- xl[yt >= min(ages)]
-	yb <- yb[yt >= min(ages)]
-	yt <- yt[yt >= min(ages)]
-	
-	# draw cohort refs:
-	if (Chrono){
-		segments(xl, yb, xr, yt, ...)
-	} else {
-		segments(rev(xr), yb, rev(xl), yt, ...)
-	}
-	
-}
+source(file.path("R","Functions.R"))
 
 # APC right angle. Go from 0 to 100 in jumps of 10 or 20.
 pdf(file.path("Figures","LabPres","APC1.pdf"),height=5,width=7)

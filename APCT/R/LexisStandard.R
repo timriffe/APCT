1b10b6d732907bc5a431ba04dbdb75cbf4e462d3
tim riffe <tim.riffe@gmail.com>
2015-07-09 18:07:46 +0200

    work from today. lab pres is finally compiling correctly

--- APCT/R/LexisStandard.R
+++ APCT/R/LexisStandard.R
@@ -18,19 +18,41 @@ getwd()
 #library(LexisUtils)
 source(file.path("R","Functions.R"))
 
-# APC right angle. Go from 0 to 100 in jumps of 10 or 20.
+# APC 1 right angle. Go from 0 to 100 in jumps of 10 or 20.
 pdf(file.path("Figures","LabPres","APC1.pdf"),height=5,width=7)
 par(mai=c(.5, .5, .5, .5), xaxs = "i", yaxs = "i")
-plot(NULL, xlim = c(1820,2000), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
 LexRefN(0:100, 1900:2000, col = "#A5A5A5", N = 20, xpd=TRUE)
 text(1900,seq(0,100,by=20),seq(0,100,by=20),pos=2,xpd=TRUE)
 text(seq(1900,2000,by=20),0,seq(1900,2000,by=20),pos=1,xpd=TRUE)
 dev.off()
 
-# APC with life lines.
+# APC with life lines 2.
 pdf(file.path("Figures","LabPres","APC2.pdf"),height=5,width=7)
 par(mai=c(.5, .5, .5, .5), xaxs = "i", yaxs = "i")
-plot(NULL, xlim = c(1820,2000), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+LexRefN(0:100, 1900:2000, col = "#A5A5A5", N = 20, xpd=TRUE)
+text(1900,seq(0,100,by=20),seq(0,100,by=20),pos=2,xpd=TRUE)
+text(seq(1900,2000,by=20),0,seq(1900,2000,by=20),pos=1,xpd=TRUE)
+# life lines
+segments(c(1900,1900,1905,1922,1965,1995),c(65,45,0,0,0,0),
+		c(1900,1900,1905,1922,1965,1995)+c(20,30,65,75,25,5),
+		c(65,45,0,0,0,0) + c(20,30,65,75,25,5), col = "blue", lwd=2)
+points(c(1905,1922,1965,1995),rep(0,4),pch=19,col="blue",xpd=TRUE)
+points(c(1900,1900,1905,1922,1965)+c(20,30,65,75,25),
+		c(65,45,0,0,0) + c(20,30,65,75,25), pch=13,cex=1.3,col="blue",lwd=2)
+#rect(1820,0,1900,100,border=NA,col="#00000020")
+dev.off()
+
+
+
+####
+#### TODO: finish right side here
+####
+# APC with life lines 3.
+pdf(file.path("Figures","LabPres","APC3.pdf"),height=5,width=7)
+par(mai=c(.5, .5, .5, .5), xaxs = "i", yaxs = "i")
+plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
 LexRefN(0:100, 1820:2000, col = "#A5A5A5", N = 20, xpd=TRUE)
 text(1820,seq(0,100,by=20),seq(0,100,by=20),pos=2,xpd=TRUE)
 text(seq(1820,2000,by=20),0,seq(1820,2000,by=20),pos=1,xpd=TRUE)
@@ -44,11 +66,30 @@ points(c(1900,1900,1905,1922,1965)+c(20,30,65,75,25),
 rect(1820,0,1900,100,border=NA,col="#00000020")
 dev.off()
 
+# APC with life lines 4.
+pdf(file.path("Figures","LabPres","APC4.pdf"),height=5,width=7)
+par(mai=c(.5, .5, .5, .5), xaxs = "i", yaxs = "i")
+plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+LexRefN(0:100, 1820:2060, col = "#A5A5A5", N = 20, xpd=TRUE)
+text(1820,seq(0,100,by=20),seq(0,100,by=20),pos=2,xpd=TRUE)
+text(seq(1820,2000,by=20),0,seq(1820,2000,by=20),pos=1,xpd=TRUE)
+# life lines
+segments(c(1920-85,1930-75,1905,1922,1965,1995),c(0,0,0,0,0,0),
+		c(1900,1900,1905,1922,1965,1995)+c(20,30,65,75,25,5),
+		c(65,45,0,0,0,0) + c(20,30,65,75,25,5), col = "blue", lwd=2)
+points(c(1920-85,1930-75,1905,1922,1965,1995),rep(0,6),pch=19,col="blue",xpd=TRUE)
+points(c(1900,1900,1905,1922,1965)+c(20,30,65,75,25),
+		c(65,45,0,0,0) + c(20,30,65,75,25), pch=13,cex=1.3,col="blue",lwd=2)
+rect(1820,0,1900,100,border=NA,col="#00000020")
+rect(2000,0,2060,100,border=NA,col="#00000020")
+
+dev.off()
+
 ##########################################################
 # TPD right angle
 pdf(file.path("Figures","LabPres","TPD1.pdf"),height=5,width=7)
 par(mai=c(.5,.5,.5,.5), xaxs = "i", yaxs = "i")
-plot(NULL, xlim = c(1820,2000), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
 LexRefN(0:100, 1900:2000, col = "#A5A5A5", N = 20, Chrono = FALSE, xpd=TRUE)
 #
 text(1900,seq(0,100,by=20),seq(0,100,by=20),pos=2,xpd=TRUE)
@@ -57,18 +98,18 @@ dev.off()
 
 pdf(file.path("Figures","LabPres","TPD2.pdf"),height=5,width=7)
 par(mai=c(.5,.5,.5,.5), xaxs = "i", yaxs = "i")
-plot(NULL, xlim = c(1820,2000), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
+plot(NULL, xlim = c(1820,2060), ylim = c(0,100), axes = FALSE, ylab = "", xlab = "", asp = 1)
 LexRefN(0:100, 1820:2000, col = "#A5A5A5", N = 20, Chrono = FALSE, xpd=TRUE)
 #
 text(1820,seq(0,100,by=20),seq(0,100,by=20),pos=2,xpd=TRUE)
 text(seq(1820,2000,by=20),0,seq(1820,2000,by=20),pos=1,xpd=TRUE)
 
 segments(c(1920-85,1930-75,1970-65,1997-75,1990-25,1995),
-		c(85,75,65,75,25,90),
+		c(85,75,65,75,25,50),
 		c(1920,1930,1970,1997,1990,2000),
-		c(0,0,0,0,0,85),col = "blue", lwd=2)
+		c(0,0,0,0,0,45),col = "blue", lwd=2)
 points(c(1920-85,1930-75,1970-65,1997-75,1990-25,1995),
-		c(85,75,65,75,25,90),pch=19,col="blue",xpd=TRUE) # births
+		c(85,75,65,75,25,50),pch=19,col="blue",xpd=TRUE) # births
 points(c(1920,1930,1970,1997,1990),
 		c(0,0,0,0,0), pch=13,cex=1.3,col="blue",lwd=2,xpd=TRUE)
 rect(1820,0,1900,100,border=NA,col="#00000020")

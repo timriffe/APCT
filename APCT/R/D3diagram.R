7ea35e4a32592e2b3f2ff526af473759f33e16ca
tim riffe <tim.riffe@gmail.com>
2015-07-16 15:54:41 +0200

    final WebGL that tweeted out

--- APCT/R/D3diagram.R
+++ APCT/R/D3diagram.R
@@ -11,7 +11,7 @@ library(rgl)
 #rgl.close()
 # graphics.off()
 plot3d(x1,y1,z1,box=FALSE,aspect=c(3,1,1),axes=FALSE,type ='n',xlab="",ylab="",zlab="")
-#decorate3d(xlim=c(1800,2100), ylim=c(0,100), zlim=c(0,100),aspect=TRUE,box = FALSE, axes = FALSE, xlab = "year",ylab = "chrono age", zlab = "thano age")
+#decorate3d(xlim=c(1800,2100), ylim=c(0,100), zlim=c(0,100),aspect=c(3,1,1),box = FALSE, axes = FALSE, xlab = "year",ylab = "chrono age", zlab = "thano age")
 
 # cohort diagonals?
 x1 <- seq(1800,2000,by=10)
@@ -22,7 +22,7 @@ y2 <- y1 + 100
 
 for (z in 0:(length(z1)-1)){
     for (i in 1:length(x1)){
-		segments3d(c(x1[i],x2[i]-10*z),c(0,100-10*z),c(10*z,10*z),col=1)
+		rgl.linestrips(c(x1[i],x2[i]-10*z),c(0,100-10*z),c(10*z,10*z),col="black",alpha = .7)
 	}
 }
 
@@ -39,7 +39,7 @@ z1 <- z2 <- rep(0,11)
 
 for (z in 0:(length(z1)-1)){
 	for (i in 1:(length(x1)-z)){
-		segments3d(c(x1[i],x2[i]),c(y1[i],y2[i]),c(10*z,10*z),col=2)
+		rgl.linestrips(c(x1[i],x2[i]),c(y1[i],y2[i]),c(10*z,10*z),col="red",alpha = .7)
 	}
 }
 
@@ -59,7 +59,7 @@ for (z in 0:(zl-1)){
 	for (i in 1:(length(x1)-z)){
 		#y22 <- y2-10*z
 		#y22 <- ifelse(sign(y22) < 0, 0, y22)
-		segments3d(c(x1[i],x2[i]),c(y1[i],y2[i]),c(10*z,10*z),col=3)
+		rgl.linestrips(c(x1[i],x2[i]),c(y1[i],y2[i]),c(10*z,10*z),col="green",alpha = .7)
 	}
 }
 
@@ -76,7 +76,7 @@ for (z in zl:0){
 		for (x in x1){
 			#y22 <- y2-10*z
 			#y22 <- ifelse(sign(y22) < 0, 0, y22)
-			segments3d(c(x+y*10,x+y*10),c(y*10,y*10),c(0,max(10*z - 10*y,0)),col=4)
+			rgl.linestrips(c(x+y*10,x+y*10),c(y*10,y*10),c(0,max(10*z - 10*y,0)),col="blue",alpha = .5)
 		}
 	}
 }
@@ -89,18 +89,63 @@ x1 <- seq(1800,2000,by=10)
 inc <- seq(10,100,by=10)
 for (iz in inc){
 	for (i in 1:length(x1)){
-		segments3d(c(x1[i],x2[i]+iz),c(0,iz),c(iz,0),col=5)
+		rgl.linestrips(c(x1[i],x2[i]+iz),c(0,iz),c(iz,0),col="magenta", alpha = .7)
 	}
 }
 
 
 # =========================================
 # Add ATL semitransparent triangle for 1900 birth cohort:
-rgl.triangles(x=c(1900,2000,1900),y=c(0,100,0),z=c(0,0,100),col="#00FF1130")
+rgl.triangles(x=c(1900,2020,1900),y=c(0,120,0),z=c(0,0,120),col="magenta",alpha = .3)
+rgl.linestrips(c(2000,2025),c(100,125),c(0,0),color = "magenta",alpha = .7)
+text3d(2030,130,0, text = "1900 cohort ATL", col="black", cex = 1.1,alpha = 1) 
+# Add ATL semitransparent triangle for 1900 period
+rgl.triangles(x=c(1920,1920,1920),y=c(0,120,0),z=c(0,0,120),col="green",alpha = .3)
+text3d(1920,125,0, text = "1920 period ATL", col="black", cex = .9,alpha = 1) 
 
 # =========================================
+# add labelling:
+
+# period
+x <- seq(1800,2100,by=10)
+texts3d(x,rep(0,length(x)),rep(0,length(x)), texts = x, col="black", adj = c(0,1), cex = .7,alpha = 1) 
+text3d(1950,-10,-10, text = "Year", col="black", cex = .9,alpha = 1) 
+# thano age
+text3d(1785,-5,50, text = "thano age", col="black", cex = 1.1,alpha = 1) 
+texts3d(1797,rep(0,11),seq(0,100,by=10), texts = seq(0,100,by=10), col="black", cex = .7,alpha = 1) 
+
+# chrono age is a bit tricky:
+
+# draw red dashed lines:
+x1 <- 1800; x2 <- seq(1800,1900,by=10)
+y1 <- y2 <- seq(0,100,by=10)
+
+for (i in 1:11){
+	rgl.linestrips(c(x1,x2[i]),c(y1[i],y2[i]),c(0,0),color = "red",alpha = 1)
+}
+texts3d(1797,seq(0,100,by=10),rep(0,11), texts = seq(0,100,by=10), col="black", cex = .7,alpha = 1) 
+text3d(1790,50,0, text = "chrono age", col="black", cex = 1.1,alpha = 1) 
+
+# do the same for period on right side:
+x1 <- x2 <- seq(2000,2100,by=10)
+y1 <- 0;y2 <- seq(0,100,by=10)
+for (i in 1:11){
+	rgl.linestrips(c(x1[i],x2[i]),c(y1,y2[i]),c(0,0),color = "green",alpha = 1)
+}
+
+# ====================================
+# add planes for start and end of observation (1900,2000), indicating past and future
+rgl.quads(x = c(1900,1900,1900,1900), y = c(-10,110,110,-10), z = c(-10,-10,110,110), col = gray(.5),alpha=.1 )
+rgl.quads(x = c(2000,2000,2000,2000), y = c(-10,110,110,-10), z = c(-10,-10,110,110), col = gray(.5),alpha=.1 )
+text3d(1900,117,117, text = "start of obs. 1900", col="black", cex = .9,alpha = 1) 
+text3d(2000,117,117, text = "end of obs. 2000", col="black", cex = .9,alpha = 1) 
+# title
+text3d(1950,0,150, text = "Tim Riffe (2015) A unified model of demographic time", col="black", cex = 1.2,alpha = 1) 
+# =========================================
 # no apply proper aspect ratio to the whole thing
-decorate3d(xlim=c(1800,2100), ylim=c(0,100), zlim=c(0,100),aspect=c(3,1,1),box = FALSE, axes = FALSE, xlab = "year",ylab = "chrono age", zlab = "thano age")
+x <- diff(par3d("bbox")[1:2])
+y <- max(c(diff(par3d("bbox")[3:4]),diff(par3d("bbox")[5:6])))
+decorate3d(xlim=c(1800,2100), ylim=c(0,100), zlim=c(0,100),aspect=c(x,y,y),box = FALSE, axes = FALSE, xlab = "",ylab = "", zlab = "")
 
 # and now we have a basic unlabeled wireframe
 

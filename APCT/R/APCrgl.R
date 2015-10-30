3312beacfee218eabf7013d543c36ffa91496908
Tim Riffe <tim.riffe@gmail.com>
2015-10-30 16:51:11 +0100

    progress on 3d APC

--- APCT/R/APCrgl.R
+++ APCT/R/APCrgl.R
@@ -16,6 +16,7 @@ if (system("hostname",intern=TRUE) %in% c("triffe-N80Vm", "tim-ThinkPad-L440")){
 	}
 }
 library(rgl)
+source("R/Functions.R")
 # need a function to take an xyz coord and transform to ternary xyz.
 xyz2ternxyz <- function(xyz){
 	ternxyz   <- xyz
@@ -25,17 +26,7 @@ xyz2ternxyz <- function(xyz){
 	ternxyz$z <- xyz$z * sqrt(6) / 3
 	ternxyz
 }
-# colors chosen by JS, his function. May as well use the same ones!
-# here: https://github.com/jschoeley/typotime/blob/master/R/diatime.R
-AssignColour <- function (x) {
-	if (x == "A") result <- "#D23737"
-	if (x == "P") result <- "#3191C9"
-	if (x == "C") result <- "#D2BC2D"
-	if (x == "T") result <- "#4EC93B"
-	if (x == "D") result <- "#881F93"
-	if (x == "L") result <- "#C5752B"
-	return(result)
-}
+
 #rgl.close()
 
 # from Josh O'Brien on SO:
@@ -48,7 +39,7 @@ AssignColour <- function (x) {
 pp <- dget("Data/TALrglview.R")
 
 
-
+rgl.close() 
 omega <- 125 # for the sake of dimensioning
 # =====================================
 # set up space, a box, a prelim aspect ratio (final adjustment later)
@@ -66,98 +57,126 @@ bg3d("white")
 maxP <- 2000
 minP <- 1700
 ns   <- seq(0,omega,by=25)
+
+# end census triangle
+xyztern <- xyz2ternxyz(data.frame(
+				x = c(maxP,maxP,maxP),
+				y = c(0, omega, 0),
+				z = c(0, 0, omega)))
+rgl.triangles(
+		x = xyztern$x,
+		y = xyztern$y, 
+		z = xyztern$z,
+		col = AssignColour("P"),
+		shininess = 0,
+		lit=FALSE,
+		alpha = .2,
+		front = "fill",
+		back = "fill")
+rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
+rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("L"),line_antialias=TRUE)
+rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = AssignColour("T"),line_antialias=TRUE)
+
+# start census triangle
+xyztern <- xyz2ternxyz(data.frame(
+				x = c(minP,minP,minP),
+				y = c(0, omega, 0),
+				z = c(0, 0, omega)))
+rgl.triangles(
+		x = xyztern$x,
+		y = xyztern$y, 
+		z = xyztern$z,
+		col = AssignColour("P"),
+		shininess = 0,
+		lit=FALSE,
+		alpha = .2,
+		front = "fill",
+		back = "fill")
+rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
+rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("L"),line_antialias=TRUE)
+rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = AssignColour("T"),line_antialias=TRUE)
+
+#rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"))
+#rgl.linestrips(xyztern$x[3:4],xyztern$y[3:4],xyztern$z[3:4],color = AssignColour("P"))
+#rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("A"))
 # an example rgl TAL (1900 birth cohort)
-for (n in ns){
-	xyztern <- xyz2ternxyz(data.frame(
-					x = c(minP, minP, maxP, maxP),
-					y = c(0, omega-n, omega-n, 0),
-					z = c(n,n,n,n)))
-	rgl.quads(
-			x = xyztern$x,
-			y = xyztern$y, 
-			z = xyztern$z,
-			col = gray(.5),
-			shininess = 0,
-			lit=FALSE,
-			alpha = .2)
-	cohorts <- seq(minP,maxP,by=25)
-	for (i in 1:length(cohorts)){
-		cohi <- xyz2ternxyz(data.frame(
-						x=c(cohorts[i],ifelse(cohorts[i]+omega-n > maxP, maxP, cohorts[i]+omega-n)),
-								y=c(0,omega-n),z=c(n,n)))
-		rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("C"))
-	}
+ #n <- 0
+ for (n in ns){
+	 xyztern <- xyz2ternxyz(data.frame(
+					 x = c(minP, minP, maxP, maxP),
+					 y = c(0, omega-n, omega-n, 0),
+					 z = c(n,n,n,n)))
+	 if (x == 25){
+	 rgl.quads(
+			 x = xyztern$x,
+			 y = xyztern$y, 
+			 z = xyztern$z,
+			 col = gray(.9),
+			 shininess = 0,
+			 lit=FALSE,
+			 alpha = .85,
+			 front = "fill",
+			 back = "fill")
+ }
+	 if (n != 25){
+		 rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
+		 rgl.linestrips(xyztern$x[3:4],xyztern$y[3:4],xyztern$z[3:4],color = AssignColour("T"))
+		 rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("A"),line_antialias=TRUE)
+		 #rgl.linestrips(xyztern$x[c(1,4)],xyztern$y[c(1,4)],xyztern$z[c(1,4)],color = AssignColour("A"),line_antialias=TRUE)
+	 }
+	 if (n == 0){
+		 rgl.linestrips(xyztern$x[c(1,4)],xyztern$y[c(1,4)],xyztern$z[c(1,4)],color = AssignColour("A"),line_antialias=TRUE)
+	 }
+	 if (n == 25){
+		 cohorts <- seq(minP-omega+n,maxP,by=25)
+		 # i <- 1
+		 for (i in 1:length(cohorts)){ 
+	         x1 <- cohorts[i]
+			 y1 <- 0
+			 if (x1 < minP){
+				 shift <- minP - x1
+				 x1 <- x1 + shift
+				 y1 <- y1 + shift
+			 }
+			 x2 <- cohorts[i] + omega - n
+			 y2 <- omega - n
+			 if (x2 > maxP){
+				 shift <- x2 - maxP
+				 x2 <- x2 - shift
+				 y2 <- y2 - shift
+			 }
+			 #(c(x1,x2,y1,y2))
+			 cohi <- xyz2ternxyz(data.frame(x=c(x1,x2),y=c(y1,y2),z=c(n,n)))
+			 rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("C"),line_antialias=TRUE)
+		 }
 # period lines
-	periods <- seq(minP,maxP,by=25)
-	for (i in 1:length(periods)){
-		cohi <- xyz2ternxyz(data.frame(x=c(periods[i],periods[i]),y=c(0,omega-n),z=c(n,n)))
-		rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("P"))
-	}
+		 periods <- seq(minP,maxP,by=25)
+		 for (i in 1:length(periods)){
+			 cohi <- xyz2ternxyz(data.frame(x=c(periods[i],periods[i]),y=c(0,omega-n),z=c(n,n)))
+			 rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("P"),line_antialias=TRUE)
+		 }
 # age lines
-	ages <- seq(0,omega-n,by=25)
-	for (i in 1:length(ages)){
-		cohi <- xyz2ternxyz(data.frame(x=c(minP,maxP),y=ages[i],z=c(n,n)))
-		rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("A"))
-	}
-}
-par3d(pp)
+		 ages <- seq(0,omega-n,by=25)
+		 for (i in 1:length(ages)){
+			 cohi <- xyz2ternxyz(data.frame(x=c(minP,maxP),y=ages[i],z=c(n,n)))
+			 rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("A"),line_antialias=TRUE)
+		 }
+	 }
+ }
 
-# add lifespan lines to 2000 TAL
-ages <- seq(0,omega,by=25)
-for (i in ages){
-	lsi <- xyz2ternxyz(data.frame(x=c(2000,2000+i),y=c(0,i),z=c(i,0)))
-	rgl.linestrips(lsi$x,lsi$y,lsi$z,color = AssignColour("L"))
-}
-# add thano lines to 2000 TAL
-for (i in ages){
-	tsi <- xyz2ternxyz(data.frame(x=c(2000,2000+i),y=c(0,i),z=omega-c(i,i)))
-	rgl.linestrips(tsi$x,tsi$y,tsi$z,color = AssignColour("T"))
-}
-# add chrono lines to 2000 TAL
-for (i in ages){
-	asi <- xyz2ternxyz(data.frame(x=c(2000+i,2000+i),y=c(i,i),z=c(0,omega-i)))
-	rgl.linestrips(asi$x,asi$y,asi$z,color = AssignColour("A"))
-}
-
-par3d(pp)
-x <- diff(par3d("bbox")[1:2])
-y <- diff(par3d("bbox")[3:4])
-z <- diff(par3d("bbox")[5:6])
-decorate3d(xlim = c(1800, 2100), ylim = c(0, 100), zlim = c(0, 100), 
-		aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
-#decorate3d(aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
-
-rgl.postscript("Figures/TALisorgl.pdf",fmt="pdf")
-rgl.snapshot("Figures/TALisorgl.png")
-#
-#rgl.material(
-#		color        = c("white"),
-#		alpha        = c(1.0),
-#		lit          = TRUE, 
-#		ambient      = "black",
-#		specular     = "white", 
-#		emission     = "black", 
-#		shininess    = 50.0, 
-#		smooth       = TRUE,
-#		texture      = NULL, 
-#		textype      = "rgb", 
-#		texmipmap    = FALSE, 
-#		texminfilter = "linear", 
-#		texmagfilter = "linear",
-#		texenvmap    = FALSE,
-#		front        = "fill", 
-#		back         = "fill",
-#		size         = 3.0, 
-#		lwd          = 1.0,
-#		fog          = TRUE,
-#		point_antialias = FALSE,
-#		line_antialias = FALSE,
-#		depth_mask   = TRUE,
-#		depth_test   = "less",
-#		...
-#)
-#(x, y = NULL, z = NULL, normals=NULL, texcoords=NULL, ... )
-
+ xyzfront <- xyz2ternxyz(data.frame(
+				 x = c(minP, maxP),
+				 y = c(0, 0),
+				 z = c(0,0)))
+ rgl.linestrips(xyzfront$x,xyzfront$y,xyzfront$z,color = AssignColour("P"),line_antialias=TRUE)
+ 
+ 
+ 
+ x <- diff(par3d("bbox")[1:2])
+ y <- diff(par3d("bbox")[3:4])
+ z <- diff(par3d("bbox")[5:6])
+ decorate3d(xlim = par3d("bbox")[1:2], par3d("bbox")[3:4], zlim = par3d("bbox")[5:6], 
+		 aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
 
 
 

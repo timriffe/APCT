6f850599eaf432e0a94e99ce18e7d4da9b6cb90c
Tim Riffe <tim.riffe@gmail.com>
2015-11-02 17:48:03 +0100

    further work on APC 3d

--- APCT/R/APCrgl.R
+++ APCT/R/APCrgl.R
@@ -36,7 +36,7 @@ xyz2ternxyz <- function(xyz){
 
 ## Save the list to a text file
 #dput(pp, file="Data/APCrglview.R", control = "all")
-pp <- dget("Data/TALrglview.R")
+pp <- dget("Data/APCrglview.R")
 
 
 rgl.close() 
@@ -55,73 +55,73 @@ plot3d(xlim = c(1800, 2100), ylim = c(0, 100), zlim = c(0, 100),
 bg3d("white")
 
 maxP <- 2000
-minP <- 1700
+minP <- 1800
 ns   <- seq(0,omega,by=25)
 
 # end census triangle
-xyztern <- xyz2ternxyz(data.frame(
-				x = c(maxP,maxP,maxP),
-				y = c(0, omega, 0),
-				z = c(0, 0, omega)))
-rgl.triangles(
-		x = xyztern$x,
-		y = xyztern$y, 
-		z = xyztern$z,
-		col = AssignColour("P"),
-		shininess = 0,
-		lit=FALSE,
-		alpha = .2,
-		front = "fill",
-		back = "fill")
-rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
-rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("L"),line_antialias=TRUE)
-rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = AssignColour("T"),line_antialias=TRUE)
+for (p in c(maxP,minP)){
 
-# start census triangle
 xyztern <- xyz2ternxyz(data.frame(
-				x = c(minP,minP,minP),
+				x = c(p,p,p),
 				y = c(0, omega, 0),
 				z = c(0, 0, omega)))
-rgl.triangles(
-		x = xyztern$x,
-		y = xyztern$y, 
-		z = xyztern$z,
-		col = AssignColour("P"),
-		shininess = 0,
-		lit=FALSE,
-		alpha = .2,
-		front = "fill",
-		back = "fill")
-rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
-rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("L"),line_antialias=TRUE)
-rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = AssignColour("T"),line_antialias=TRUE)
+rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("L"),line_antialias=TRUE,lwd=2)
+rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("A"),line_antialias=TRUE,lwd=2)
+rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = AssignColour("T"),line_antialias=TRUE,lwd=2)
+
+Lsi <- seq(25,100,by=25)
+for (L in Lsi){
+	lsi <- xyz2ternxyz(data.frame(
+					x = c(p,p),
+					y = c(L, 0),
+					z = c(0, L)))
+	rgl.linestrips(lsi$x,lsi$y,lsi$z,color = AssignColour("L"),line_antialias=TRUE)
+}
+Asi <- seq(25,100,by=25)
+for (A in Asi){
+	asi <- xyz2ternxyz(data.frame(
+					x = c(p,p),
+					y = c(A, A),
+					z = c(0, omega-A)))
+	rgl.linestrips(asi$x,asi$y,asi$z,color = AssignColour("A"),line_antialias=TRUE)
+}
+Tsi <- seq(25,100,by=25)
+for (TT in Tsi){
+	tsi <- xyz2ternxyz(data.frame(
+					x = c(p,p),
+					y = c(0, omega-TT),
+					z = c(TT, TT)))
+	rgl.linestrips(tsi$x,tsi$y,tsi$z,color = AssignColour("T"),line_antialias=TRUE)
+}
+}
 
 #rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"))
 #rgl.linestrips(xyztern$x[3:4],xyztern$y[3:4],xyztern$z[3:4],color = AssignColour("P"))
 #rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("A"))
 # an example rgl TAL (1900 birth cohort)
  #n <- 0
+
  for (n in ns){
 	 xyztern <- xyz2ternxyz(data.frame(
 					 x = c(minP, minP, maxP, maxP),
 					 y = c(0, omega-n, omega-n, 0),
 					 z = c(n,n,n,n)))
-	 if (x == 25){
-	 rgl.quads(
-			 x = xyztern$x,
-			 y = xyztern$y, 
-			 z = xyztern$z,
-			 col = gray(.9),
-			 shininess = 0,
-			 lit=FALSE,
-			 alpha = .85,
-			 front = "fill",
-			 back = "fill")
- }
+#	 if (n == 25){
+#	 rgl.quads(
+#			 x = xyztern$x,
+#			 y = xyztern$y, 
+#			 z = xyztern$z,
+#			 col = gray(.9),
+#			 shininess = 0,
+#			 lit=FALSE,
+#			 alpha = .5,
+#			 front = "fill",
+#			 back = "fill")
+# }
 	 if (n != 25){
-		 rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
+		 rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("T"),line_antialias=TRUE)
 		 rgl.linestrips(xyztern$x[3:4],xyztern$y[3:4],xyztern$z[3:4],color = AssignColour("T"))
-		 rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("A"),line_antialias=TRUE)
+		 rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("T"),line_antialias=TRUE)
 		 #rgl.linestrips(xyztern$x[c(1,4)],xyztern$y[c(1,4)],xyztern$z[c(1,4)],color = AssignColour("A"),line_antialias=TRUE)
 	 }
 	 if (n == 0){
@@ -146,37 +146,65 @@ rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = Ass
 				 y2 <- y2 - shift
 			 }
 			 #(c(x1,x2,y1,y2))
-			 cohi <- xyz2ternxyz(data.frame(x=c(x1,x2),y=c(y1,y2),z=c(n,n)))
+			 cohi <- xyz2ternxyz(data.frame(x=c(x1,x2),y=c(y1,y2),z=c(n,n)+0.1))
 			 rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("C"),line_antialias=TRUE)
 		 }
 # period lines
-		 periods <- seq(minP,maxP,by=25)
+		 periods <- seq(minP+25,maxP,by=25)
 		 for (i in 1:length(periods)){
-			 cohi <- xyz2ternxyz(data.frame(x=c(periods[i],periods[i]),y=c(0,omega-n),z=c(n,n)))
-			 rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("P"),line_antialias=TRUE)
+			 peri <- xyz2ternxyz(data.frame(x=c(periods[i],periods[i]),y=c(0,omega-n),z=c(n,n)+0.1))
+			 rgl.linestrips(peri$x,peri$y,peri$z,color = AssignColour("P"),line_antialias=TRUE)
 		 }
 # age lines
-		 ages <- seq(0,omega-n,by=25)
+		 ages <- seq(25,omega-n,by=25)
 		 for (i in 1:length(ages)){
-			 cohi <- xyz2ternxyz(data.frame(x=c(minP,maxP),y=ages[i],z=c(n,n)))
-			 rgl.linestrips(cohi$x,cohi$y,cohi$z,color = AssignColour("A"),line_antialias=TRUE)
+			 agei <- xyz2ternxyz(data.frame(x=c(minP,maxP),y=ages[i],z=c(n,n)+0.1))
+			 rgl.linestrips(agei$x,agei$y,agei$z,color = AssignColour("A"),line_antialias=TRUE)
 		 }
+		 # period axis
+	 
 	 }
  }
-
+ peri <- xyz2ternxyz(data.frame(x=c(minP,minP),y=c(0,omega-25),z=c(25,25)))
+ rgl.linestrips(peri$x,peri$y,peri$z,color = AssignColour("A"),line_antialias=TRUE, lwd =2)
+ agei <- xyz2ternxyz(data.frame(x=c(minP,maxP),y=0,z=c(25,25)))
+ rgl.linestrips(agei$x,agei$y,agei$z,color = AssignColour("P"),line_antialias=TRUE,lwd=2)
+ 
  xyzfront <- xyz2ternxyz(data.frame(
 				 x = c(minP, maxP),
 				 y = c(0, 0),
 				 z = c(0,0)))
- rgl.linestrips(xyzfront$x,xyzfront$y,xyzfront$z,color = AssignColour("P"),line_antialias=TRUE)
- 
- 
- 
- x <- diff(par3d("bbox")[1:2])
- y <- diff(par3d("bbox")[3:4])
- z <- diff(par3d("bbox")[5:6])
- decorate3d(xlim = par3d("bbox")[1:2], par3d("bbox")[3:4], zlim = par3d("bbox")[5:6], 
-		 aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
+ rgl.linestrips(xyzfront$x,xyzfront$y,xyzfront$z,color = AssignColour("T"),line_antialias=TRUE)
+ # start census triangle
+# xyztern <- xyz2ternxyz(data.frame(
+#				 x = c(minP,minP,minP),
+#				 y = c(0, omega, 0),
+#				 z = c(0, 0, omega)))
+# #rgl.triangles(
+##		 x = xyztern$x,
+##		 y = xyztern$y, 
+##		 z = xyztern$z,
+##		 col = AssignColour("P"),
+##		 shininess = 0,
+##		 lit=FALSE,
+##		 alpha = .2,
+##		 front = "fill",
+##		 back = "fill")
+# rgl.linestrips(xyztern$x[1:2],xyztern$y[1:2],xyztern$z[1:2],color = AssignColour("P"),line_antialias=TRUE)
+# rgl.linestrips(xyztern$x[2:3],xyztern$y[2:3],xyztern$z[2:3],color = AssignColour("L"),line_antialias=TRUE)
+# rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = AssignColour("T"),line_antialias=TRUE)
+# 
 
+ 
+ 
+ 
+ par3d(pp)
+# x <- diff(par3d("bbox")[1:2])
+# y <- diff(par3d("bbox")[3:4])
+# z <- diff(par3d("bbox")[5:6])
+# decorate3d(xlim = par3d("bbox")[1:2], par3d("bbox")[3:4], zlim = par3d("bbox")[5:6], 
+#		 aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
 
+rgl.postscript("Figures/APCisorgl.pdf",fmt="pdf")
+rgl.snapshot("Figures/APCisorgl.png")
 

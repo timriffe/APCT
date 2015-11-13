0b732bae80b10ef81ae7d23be5d0df0c165778bc
Tim Riffe <tim.riffe@gmail.com>
2015-11-13 13:32:57 +0100

    Now we have an axis direction generator!

--- APCT/R/Logic.R
+++ APCT/R/Logic.R
@@ -39,7 +39,7 @@ LCD <- function(set){
 	set
 }
 
-Identity <- function(set){
+Identity <- function(set, verbose = TRUE){
 	require(magrittr)
 	set   <- toupper(set)
 	stopifnot(all(set %in% c("A","P","C","T","D","L")))
@@ -52,9 +52,11 @@ Identity <- function(set){
 		if (ni > NI)  NI <- ni
 		
 	}
-	if (length(set) == 2) cat("uninformative dyad\n")
-	if (length(set) == 3) cat("triad identity\n")
-	if (length(set) == 6) cat("hexad identity\n")
+	if (verbose){
+		if (length(set) == 2) cat("uninformative dyad\n")
+		if (length(set) == 3) cat("triad identity\n")
+		if (length(set) == 6) cat("hexad identity\n")
+	}
 	sort(set)
 }
 
@@ -67,3 +69,89 @@ Identity <- function(set){
 # Identity(c("A","T","L"))
 # hexad identity
 # Identity(c("A","T","D"))
+
+# now to figure out diagonal direction (upward or downward slope)
+
+# A + C == P
+# A + T == L
+# P + T == D
+# L + C == D
+
+# a function to get the derived measure, if any
+derived <- function(set){
+	set   <- toupper(set)
+	setdiff(Identity(set,FALSE),set)
+}
+
+# check set membership while maintaining dimensions of left side matrix
+`%indim%` <- function(mat,x){
+	hm <- mat %in% x
+	dim(hm) <- dim(mat)
+	hm
+}
+
+# return a list with x,y, derived (if any), and whether the derived measure is increasing or decreasing.
+dyadaxes <- function(x = "P", y = "A", verbose = TRUE){
+	dyad <- c(x, y)
+	der  <- derived(dyad)
+	if (length(der) == 0){
+		if (verbose){
+			cat("Diagram\n",
+					y,"|           \n",
+					" |           \n",
+					" |           \n",
+					" |           \n",
+					" |___________\n",
+					"       ", x, "\n"
+					, sep = "")
+		}
+		return(invisible(list(x = x, y = y, derived = NA, increasing = NA)))
+	}
+	this.order <- c(x, y, der)
+	# these are the orderings: always 
+	# A + C == P
+	# A + T == L
+	# P + T == D
+	# L + C == D
+	ids.ordered <- matrix(c("A", "C", "P",
+					"A", "T", "L",
+					"P", "T", "D",
+					"L", "C", "D"), nrow = 4, byrow = TRUE)
+	id <- which(rowSums(ids.ordered %indim% this.order) == 3)
+	base.order <- ids.ordered[id, ]
+	xi <- which(base.order == x)
+	yi <- which(base.order == y)
+	# if xi & yi are < 3, then it's a descending diagonal
+	# otherwise increasing upward to right.
+	if (all(c(xi,yi) < 3)){
+		# descending
+		if (verbose){
+			cat("Diagram\n",
+					y,"|⋱          \n",
+					" |  ⋱       \n",
+					" |    ⋱  ", der, "\n",
+					" |      ⋱ \n",
+					" |________⋱\n",
+					"       ", x, "\n"
+					, sep = "")
+		}
+		return(invisible(list(x = x, y = y, derived = der, increasing = FALSE)))
+	} else {
+		# increasing
+		if (verbose){
+			cat("Diagram\n",
+					y,"|        ⋰  \n",
+					" |      ⋰   \n",
+					" |    ⋰ ", der, "\n",
+					" |  ⋰     \n",
+					" |⋰__________\n",
+					"       ", x, "\n"
+					, sep = "")
+		}
+		return(invisible(list(x = x, y = y, derived = der, increasing = TRUE)))
+	}
+}
+
+
+#dyadaxes("L","D")
+

6757ca25caf8785275db085e2a6581886284ba7f
Tim Riffe <tim.riffe@gmail.com>
2016-02-24 14:55:23 +0100

    gonig to redo diatime, change style a bit

---
+++ APCT/R/DiagramTable.R
@@ -0,0 +1,18 @@
+
+# this script uses the HMDresults object to search for common patterns to the various defined measures.
+if (system("hostname",intern=TRUE) %in% c("triffe-N80Vm", "tim-ThinkPad-L440")){
+	# if I'm on the laptop
+	setwd("/home/tim/git/APCT/APCT")
+} else {
+	if (system("hostname",intern=TRUE) == "PC-403478"){
+		# on MPIDR PC
+		setwd("U://git//APCT//APCT")
+	} else {
+		# in that case I'm on Berkeley system, and other people in the dept can run this too
+		setwd(paste0("/data/commons/",system("whoami",intern=TRUE),"/git/APCT/APCT"))
+	}
+}
+
+source("R/Functions.R")
+
+

db24b81ff381308114d1973bad5bd45ff8eecc9e
Tim Riffe <tim.riffe@gmail.com>
2016-03-16 13:49:19 +0100

    have made first draft of wireframe portion of figure, but ran into Inkscape problems with transparent plane layering.. to be continued

--- APCT/R/APCrgl2.R
+++ APCT/R/APCrgl2.R
@@ -25,8 +25,8 @@ source("R/Functions.R")
 #pp <- par3d(no.readonly=TRUE)
 #pp <- par3d(no.readonly=TRUE)
 ## Save the list to a text file
-#dput(pp, file="Data/TALrglview2.R", control = "all")
-pp <- dget("Data/TALrglview2.R")
+#dput(pp, file="Data/APCrglview2.R", control = "all")
+pp <- dget("Data/APCrglview2.R")
 
 startYr <- 1800
 endYr <- 2100
@@ -47,18 +47,22 @@ plot3d(xlim = c(startYr, endYr), ylim = c(0, omega), zlim = c(0, omega),
 bg3d("white")
 # now 3 calendar lines
 cal1         <- xyz2ternxyz(data.frame(x = c(startYr, endYr), y = c(0, 0), z = c(0, 0)))
-leftagebound <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, omega), z = c(0, 0)))
+leftagebound <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, omega-50), z = c(50, 50)))
 thanoaxis    <- xyz2ternxyz(data.frame(x = c(startYr, startYr), y = c(0, 0), z = c(0, omega)))
 #cal2 <- xyz2ternxyz(data.frame(x = c(startYr, endYr), y = c(omega, omega), z = c(0, 0)))
 cal3 <- xyz2ternxyz(data.frame(x = c(startYr, endYr-100), y = c(0,0), z = c(omega, omega)))
 rgl.linestrips(cal1$x, cal1$y, cal1$z, color = AssignColour("P"), lwd = 2)
-rgl.linestrips(leftagebound$x, leftagebound$y, leftagebound$z, color = AssignColour("A"), lwd = 2)
+#rgl.linestrips(leftagebound$x, leftagebound$y, leftagebound$z, color = AssignColour("A"), lwd = 2)
 #rgl.linestrips(cal2$x, cal2$y, cal2$z, color = AssignColour("P"), lwd = 2)
 rgl.linestrips(cal3$x, cal3$y, cal3$z, color = AssignColour("P"), lwd = 2)
 rgl.linestrips(thanoaxis$x, thanoaxis$y, thanoaxis$z, color = AssignColour("T"), lwd = 2)
 # add an apc plane on the bottom
 # birth cohort lines
 z <- 50 # how high to draw apc plane
+#whitesurface <- xyz2ternxyz(data.frame(x=c(startYr,startYr,endYr,endYr+omega-z),
+#				y=c(omega-z,0,0,omega-z),z=c(z,z,z,z)-.02))
+#rgl.quads(whitesurface$x,whitesurface$y,whitesurface$z,alpha=.7,color="white",
+#		ambient="white",specular="white",emission="white", sinyness=0)
 cohorts <- seq(startYr-omega+z, endYr, by = 25)
 for (i in 1:length(cohorts)){
 	cohi <- xyz2ternxyz(data.frame(x = c(ifelse(cohorts[i]<startYr,startYr,cohorts[i]), cohorts[i] + omega - z), 
@@ -67,7 +71,7 @@ for (i in 1:length(cohorts)){
 }
 # complete periods into future
 # period lines
-periods <- seq(startYr+25, endYr + omega - z, by = 25)
+periods <- seq(startYr, endYr + omega - z, by = 25)
 for (i in 1:length(periods)){
 	cohi <- xyz2ternxyz(data.frame(x = c(periods[i], periods[i]), 
 					y = c(ifelse(periods[i]<endYr,0,periods[i]-endYr), omega-z), z = c(z, z)))
@@ -95,6 +99,8 @@ cohs <- seq(1800,1850,by=25)
 						x = c(co, co+omega, co) ,
 						y = c(0, omega, 0),
 						z = c(0, 0, omega)))
+		#rgl.triangles(xyztern$x,xyztern$y,xyztern$z,alpha=0.1,color = gray(.8),
+		#		ambient="black",specular="black",emission="black",shininess=0)
 		rgl.linestrips(xyztern$x[c(1,2)],xyztern$y[c(1,2)],xyztern$z[c(1,2)],color = gray(.5))
 		rgl.linestrips(xyztern$x[c(2,3)],xyztern$y[c(2,3)],xyztern$z[c(2,3)],color = gray(.5))
 		rgl.linestrips(xyztern$x[c(1,3)],xyztern$y[c(1,3)],xyztern$z[c(1,3)],color = gray(.5))
@@ -105,6 +111,8 @@ for (z in zs){
 					x = c(1800, 1800, 2100, 2100+omega-z),
 					y = c(omega-z,0, 0, omega-z),
 					z = c(z, z, z,z)))
+	#rgl.quads(xyztern$x,xyztern$y,xyztern$z,alpha=0.3,color = gray(.8),
+	#		ambient="black",specular="white",emission="black")
 	# left
 	rgl.linestrips(xyztern$x[c(1,2)],xyztern$y[c(1,2)],xyztern$z[c(1,2)],color = gray(.5))
 	# bottom
@@ -113,29 +121,41 @@ for (z in zs){
 	rgl.linestrips(xyztern$x[c(3,4)],xyztern$y[c(3,4)],xyztern$z[c(3,4)],color = gray(.5))
 	# back
 	rgl.linestrips(xyztern$x[c(4,1)],xyztern$y[c(4,1)],xyztern$z[c(4,1)],color = gray(.5))
-	
+}
+# more APC planes in the back, cut off
+zs <- c(75,100)	
+for (z in zs){
+	xyztern <- xyz2ternxyz(data.frame(
+					x = c(1800, 1800, 1900, 1900+omega-z),
+					y = c(omega-z,0, 0, omega-z),
+					z = c(z, z, z,z)))
+	#rgl.quads(xyztern$x,xyztern$y,xyztern$z,alpha=0.3,color = gray(.5),
+	#		ambient="black",specular="white",emission="black")
+	# left
+	rgl.linestrips(xyztern$x[c(1,2)],xyztern$y[c(1,2)],xyztern$z[c(1,2)],color = gray(.5))
+	# bottom
+	rgl.linestrips(xyztern$x[c(2,3)],xyztern$y[c(2,3)],xyztern$z[c(2,3)],color = gray(.5))
+	#right
+	rgl.linestrips(xyztern$x[c(3,4)],xyztern$y[c(3,4)],xyztern$z[c(3,4)],color = gray(.5))
+	# back
+	rgl.linestrips(xyztern$x[c(4,1)],xyztern$y[c(4,1)],xyztern$z[c(4,1)],color = gray(.5))
 }
 
-	
+par3d(pp)
+rgl.postscript("Figures/APCisorgl2.1.pdf",fmt="pdf")
 
-# add lifespan lines to 2000 TAL
-ages <- seq(0,omega,by=25)
-for (i in ages){
-	lsi <- xyz2ternxyz(data.frame(x=c(2000,2000+i),y=c(0,i),z=c(i,0)))
-	rgl.linestrips(lsi$x,lsi$y,lsi$z,color = AssignColour("L"))
-}
-# add thano lines to 2000 TAL
-for (i in ages){
-	tsi <- xyz2ternxyz(data.frame(x=c(2000,2000+i),y=c(0,i),z=omega-c(i,i)))
-	rgl.linestrips(tsi$x,tsi$y,tsi$z,color = AssignColour("T"))
-}
-# add chrono lines to 2000 TAL
-for (i in ages){
-	asi <- xyz2ternxyz(data.frame(x=c(2000+i,2000+i),y=c(i,i),z=c(0,omega-i)))
-    rgl.linestrips(asi$x,asi$y,asi$z,color = AssignColour("A"))
-}
 
-#par3d(pp)
+
+
+
+
+
+
+
+
+
+
+
 x <- diff(par3d("bbox")[1:2])
 y <- diff(par3d("bbox")[3:4])
 z <- diff(par3d("bbox")[5:6])
@@ -143,34 +163,5 @@ decorate3d(xlim = c(startYr, endYr), ylim = c(0, omega-25), zlim = c(0, omega-25
 		aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
 #decorate3d(aspect = c(x, y, z), box = FALSE, axes = FALSE, xlab = "", ylab = "", zlab = "")
 
-rgl.postscript("Figures/TALisorgl2.pdf",fmt="pdf")
-rgl.snapshot("Figures/TALisorgl.png")
-#
-#rgl.material(
-#		color        = c("white"),
-#		alpha        = c(1.0),
-#		lit          = TRUE, 
-#		ambient      = "black",
-#		specular     = "white", 
-#		emission     = "black", 
-#		shininess    = 50.0, 
-#		smooth       = TRUE,
-#		texture      = NULL, 
-#		textype      = "rgb", 
-#		texmipmap    = FALSE, 
-#		texminfilter = "linear", 
-#		texmagfilter = "linear",
-#		texenvmap    = FALSE,
-#		front        = "fill", 
-#		back         = "fill",
-#		size         = 3.0, 
-#		lwd          = 1.0,
-#		fog          = TRUE,
-#		point_antialias = FALSE,
-#		line_antialias = FALSE,
-#		depth_mask   = TRUE,
-#		depth_test   = "less",
-#		...
-#)
-#(x, y = NULL, z = NULL, normals=NULL, texcoords=NULL, ... )
+rgl.postscript("Figures/APCisorgl2.2.pdf",fmt="pdf")
 
